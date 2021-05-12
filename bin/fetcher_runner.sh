90df50eae6426550f2d79ff1aa43c30221616823
Tigran Kalaydzhyan <kalaydzhyan@gmail.com>
2021-05-12 00:39:10 -0500

    runner with a proper path

--- bin/fetcher_runner.sh
+++ bin/fetcher_runner.sh
@@ -1,1 +1,3 @@
-for i in {1..10}; do ./fetcher_taylor.py -begin_id=$((i*10000)) -end_id=$(( (i+1)*10000)) &>/dev/null & done
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+for i in {1..10}; do $SCRIPT_DIR/fetcher_taylor.py -begin_id=$((i*10000)) -end_id=$(( (i+1)*10000)) & done
