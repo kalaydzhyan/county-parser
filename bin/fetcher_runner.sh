7a4ce4e321332bde33184b208fc00dd0a9120c96
Tigran Kalaydzhyan <kalaydzhyan@gmail.com>
2021-05-28 02:16:32 -0500

    typo in fetcher_runner

--- bin/fetcher_runner.sh
+++ bin/fetcher_runner.sh
@@ -3,5 +3,5 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 for i in {1..10}; do $SCRIPT_DIR/fetcher_taylor.py -begin_id=$((i*10000)) -end_id=$(( (i+1)*10000)) & done
 $SCRIPT_DIR/fetcher_callahan.py &
 $SCRIPT_DIR/fetcher_jones.py --owners &
-$SCRIPT_DIR/fetcher_callahan.py --properties &
+$SCRIPT_DIR/fetcher_jones.py --properties &
 
