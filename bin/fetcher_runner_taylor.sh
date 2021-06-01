eefbb7b229ae25a9273ea7127b72ca772e6a86e6
Tigran Kalaydzhyan <kalaydzhyan@gmail.com>
2021-06-01 14:16:17 -0500

    fetcher_runner for taylor only

--- bin/fetcher_runner_taylor.sh
+++ bin/fetcher_runner_taylor.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 for i in {1..10}; do $SCRIPT_DIR/fetcher_taylor.py -begin_id=$((i*10000)) -end_id=$(( (i+1)*10000)) & done
-$SCRIPT_DIR/fetcher_callahan.py &
-$SCRIPT_DIR/fetcher_jones.py --owners &
-$SCRIPT_DIR/fetcher_jones.py --properties &
 
