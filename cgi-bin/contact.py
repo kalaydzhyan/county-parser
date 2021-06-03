16fa1e84e6398dd2024d2e2a482a7c788f19f67d
Tigran Kalaydzhyan <kalaydzhyan@gmail.com>
2021-06-03 17:16:47 -0500

    working email contact form

--- cgi-bin/contact.py
+++ cgi-bin/contact.py
@@ -2,21 +2,21 @@
 
 import boto3
 from botocore.exceptions import ClientError
-import cgi, cgitb, time
-
+import cgi, cgitb
 
 form    = cgi.FieldStorage() 
 name    = form.getvalue('name')
 email   = form.getvalue('email')
 SUBJECT = form.getvalue('subject')
 message = form.getvalue('message')
-print("Status: 200 OK\n")
 
+#name = email = SUBJECT = message = 'TEST'
+
+print("Status: 200 OK\n")
 
 SENDER     = "Abilene Rental Homes <abilene.rental.homes@gmail.com>"
 RECIPIENT  = "abilene.rental.homes@gmail.com"
-AWS_REGION = "us-east-2"
-BODY_TEXT  = (f"  {name} with email {email} sent you the following message: {message}"
+BODY_TEXT  = (f"{name} with email {email} sent you the following message: {message}"
               "This email was sent with Amazon SES using the "
               "AWS SDK for Python (Boto)."
              )
@@ -28,8 +28,9 @@ BODY_HTML = f"""<html>
 <head></head>
 <body>
   Hi Tigran,
- 
+  <br>
   {name} with email {email} sent you the following message:
+  <br>
   {message}
 
   <p>This email was sent with
@@ -38,15 +39,25 @@ BODY_HTML = f"""<html>
       AWS SDK for Python (Boto)</a>.</p>
 </body>
 </html>
-            """            
+"""
 
-# The character encoding for the email.
+DEFAULT_REGION = 'us-east-2'
 CHARSET = "UTF-8"
 
-# Create a new SES resource and specify a region.
-client = boto3.client('ses',region_name=AWS_REGION)
 
-# Try to send the email.
+aws_dict = {'region': DEFAULT_REGION}
+
+for fname in ['config', 'credentials']:
+    with open(fname, 'r') as f:
+        for line in f:
+            if '=' in line:
+                key, val = line.split('=')
+                aws_dict.update({key.strip(): val.strip()})
+
+client = boto3.client('ses', region_name=aws_dict['region'],
+                            aws_access_key_id=aws_dict['aws_access_key_id'],
+                            aws_secret_access_key=aws_dict['aws_secret_access_key'])
+
 try:
     #Provide the contents of the email.
     response = client.send_email(
@@ -81,3 +92,4 @@ except ClientError as e:
     print(e.response['Error']['Message'])
 else:
     print('OK\n')
+
