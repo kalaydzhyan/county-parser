50dbaa36eb342e7004ac61047681e33bbd532824
Tigran Kalaydzhyan <kalaydzhyan@gmail.com>
2021-06-07 00:46:39 -0500

    misc service files

---
+++ bin/aws_startup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+yum update -y
+yum install emacs -y
+yum install mc -y
+yum install git -y
+yum install pip -y
+yum install ghostscript -y
+
+cd /tmp/
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+pip install awsebcli --upgrade --user
+
+for name in requests bs4 numpy pandas tqdm regex xldr lxml boto3; do pip install $name; done
+
+cd /tmp/
+wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+curl https://intoli.com/install-google-chrome.sh | bash
+sudo mv /usr/bin/google-chrome-stable /usr/bin/google-chrome
+pip install selenium
+pip install webdriver-manager
+
+yum install httpd -y
+systemctl start httpd
+systemctl enable httpd
+cd /var/www/html
+echo "Privet! This is a test page running on Apache in the AWS Cloud" > index.html
---
+++ update_www.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/bash
+cp -r /home/ec2-user/county-parser/html/* /var/www/html/
+cp -r /home/ec2-user/county-parser/cgi-bin/* /var/www/cgi-bin/
