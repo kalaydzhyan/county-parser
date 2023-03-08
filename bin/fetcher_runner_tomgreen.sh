cdbddb2a6e4d8d8253bc7bc390e6a67386eee1ff
Tigran Kalaydzhyan <kalaydzhyan@gmail.com>
2023-03-08 16:24:01 -0600

    tomgreen fetcher runner

--- bin/fetcher_runner_tomgreen.sh
+++ bin/fetcher_runner_tomgreen.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-for i in {0..120}; do $SCRIPT_DIR/fetcher_taylor.py -begin_id=$((i*1000)) -end_id=$(( (i+1)*1000)) & done
+for i in {0..5}; do $SCRIPT_DIR/fetcher_tomgreen.py -begin_id=$((i*20000)) -end_id=$(( (i+1)*20000)) & done
